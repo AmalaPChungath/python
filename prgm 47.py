7a849708cc9e63b93057b64160c8e8146835f898
AmalaPChungath <147935213+AmalaPChungath@users.noreply.github.com>
2023-11-03 15:23:31 +0530

    Add files via upload

--- prgm 47.py
+++ prgm 47.py
@@ -1,5 +1,7 @@
 def rev_str(x):
-    if not len(x):return ''
-    else:return x[-1]+rev_str(x[::-1])
+    if not len(x):
+        return ''
+    else:
+        return x[-1]+rev_str(x[:-1])
 s=input('Enter a string: ')
 print('Reversed string = ',rev_str(s))
